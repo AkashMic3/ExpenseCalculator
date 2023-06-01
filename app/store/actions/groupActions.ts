8c8698bcd1a468914d7f15c29156bcc3a8f14f9e
akash <akash.micheal@qburst.com>
2023-06-01 14:52:20 +0530

    fetch members for add

--- app/store/actions/groupActions.ts
+++ app/store/actions/groupActions.ts
@@ -1,6 +1,5 @@
+import { member } from 'app/models/actions/group';
 import * as types from './types';
-
-
 
 export function fetchGroupMembers(group_Id: string) {
   return {
@@ -9,30 +8,40 @@ export function fetchGroupMembers(group_Id: string) {
   };
 }
 
-export function addGroup(group_name: string, members: [any], owner_id:string, created_at:string) {
-    return {
-      type: types.ADD_GROUP,
-      group_name,
-      members,
-      owner_id,
-      created_at
-    };
-  }
-  
+export function addGroup(
+  group_name: string,
+  members: [any],
+  owner_id: string,
+  created_at: string,
+) {
+  return {
+    type: types.ADD_GROUP,
+    group_name,
+    members,
+    owner_id,
+    created_at,
+  };
+}
 
-  export function fetchGroups(user_id:string) {
-    return {
-      type: types.FETCH_GROUPS,
-      user_id
-    };
-  }
+export function fetchGroups(user_id: string) {
+  return {
+    type: types.FETCH_GROUPS,
+    user_id,
+  };
+}
 
-  export function onfetchGroupResponse(response: any) {
-    console.log("fetch group response:", response)
-    return {
-      type: types.FETCH_GROUPS_RESPONSE,
-      response,
-    };
-  }
-  
+export function onfetchGroupResponse(response: any) {
+  console.log('fetch group response:', response);
+  return {
+    type: types.FETCH_GROUPS_RESPONSE,
+    response,
+  };
+}
 
+export function setGroupMembers(members: [member]) {
+  console.log(members, 'members');
+  return {
+    type: types.SET_MEMBERS,
+    members,
+  };
+}
